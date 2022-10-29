7264f5773d20a2eeb64f3d8c3c15b2012d752ad3
HAO YANG <lkeki2015@gmail.com>
2022-10-29 11:31:51 +0800

    first commit

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -29,8 +29,7 @@ const Product = () => {
             url:apiUrl+"/api/publicproduct",
             headers: {
                 'Access-Control-Allow-Origin' : '*',
-                'Access-Control-Allow-Methods':' GET, POST, PATCH, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers':' Origin, Content-Type, X-Auth-Token'
+                "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
             }
         })
         .then((res)=>(
